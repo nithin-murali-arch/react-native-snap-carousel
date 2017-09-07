fdc1e908eadca4f83e34218d134b520070f77c49
Nithin <nithin93@gmail.com>
2017-09-07 15:52:33 +0530

    Add files via upload

--- src/pagination/Pagination.js
+++ src/pagination/Pagination.js
@@ -1,125 +1,72 @@
-import React, { PureComponent } from 'react';
-import { View, ViewPropTypes } from 'react-native';
+import React, { Component } from 'react';
+import { View, ScrollView, TouchableOpacity, ViewPropTypes } from 'react-native';
 import PropTypes from 'prop-types';
 import PaginationDot from './PaginationDot';
 import styles from './Pagination.style';
 
-export default class Pagination extends PureComponent {
+export default class Pagination extends Component {
 
     static propTypes = {
-        activeDotIndex: PropTypes.number.isRequired,
         dotsLength: PropTypes.number.isRequired,
-        carouselRef: PropTypes.object,
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        dotColor: PropTypes.string,
-        dotElement: PropTypes.element,
-        dotStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        inactiveDotColor: PropTypes.string,
-        inactiveDotElement: PropTypes.element,
+        activeDotIndex: PropTypes.number.isRequired,
+        containerStyle: ViewPropTypes.style,
+        dotStyle: ViewPropTypes.style,
         inactiveDotOpacity: PropTypes.number,
-        inactiveDotScale: PropTypes.number,
-        inactiveDotStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
-        renderDots: PropTypes.func,
-        tappableDots: PropTypes.bool,
-        vertical: PropTypes.bool
+        inactiveDotScale: PropTypes.number
     };
-
-    static defaultProps = {
-        inactiveDotOpacity: 0.5,
-        inactiveDotScale: 0.5,
-        tappableDots: false,
-        vertical: false
-    }
-
-    constructor (props) {
-        super(props);
-
-        // Warnings
-        if ((props.dotColor && !props.inactiveDotColor) || (!props.dotColor && props.inactiveDotColor)) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You need to specify both `dotColor` and `inactiveDotColor`'
-            );
-        }
-        if ((props.dotElement && !props.inactiveDotElement) || (!props.dotElement && props.inactiveDotElement)) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You need to specify both `dotElement` and `inactiveDotElement`'
-            );
-        }
-        if (props.tappableDots && !props.carouselRef) {
-            console.warn(
-                'react-native-snap-carousel | Pagination: ' +
-                'You must specify prop `carouselRef` when setting `tappableDots` to `true`'
-            );
-        }
-    }
 
     get dots () {
         const {
-            activeDotIndex,
-            carouselRef,
             dotsLength,
-            dotColor,
-            dotElement,
+            activeDotIndex,
             dotStyle,
-            inactiveDotColor,
-            inactiveDotElement,
             inactiveDotOpacity,
             inactiveDotScale,
-            inactiveDotStyle,
-            renderDots,
-            tappableDots
+            parentThis,
+            colorsArray
         } = this.props;
 
-        if (renderDots) {
-            return renderDots(activeDotIndex, dotsLength, this);
-        }
-
-        const DefaultDot = <PaginationDot
-          carouselRef={carouselRef}
-          tappable={tappableDots && typeof carouselRef !== 'undefined'}
-          color={dotColor}
-          style={dotStyle}
-          inactiveColor={inactiveDotColor}
-          inactiveOpacity={inactiveDotOpacity}
-          inactiveScale={inactiveDotScale}
-          inactiveStyle={inactiveDotStyle}
-        />;
-
         let dots = [];
-
+        var onPressFunction = function(index){
+            //var index = parseInt(key.props.children.key.split("-")[2]);
+            //index = parseInt(index);
+            parentThis.snapToItem(index, true); 
+            parentThis.setState({ activeSlide: index });
+        }
         for (let i = 0; i < dotsLength; i++) {
-            const isActive = i === activeDotIndex;
-            dots.push(React.cloneElement(
-                (isActive ? dotElement : inactiveDotElement) || DefaultDot,
-                {
-                    key: `pagination-dot-${i}`,
-                    active: i === activeDotIndex,
-                    index: i
-                }
-            ));
+            dots.push(
+                <TouchableOpacity key={i} onPress={() => onPressFunction(i)}> 
+                <PaginationDot
+                  key={`pagination-dot-${i}`}
+                  active={i === activeDotIndex}
+                  style={dotStyle}
+                  inactiveOpacity={inactiveDotOpacity}
+                  inactiveScale={inactiveDotScale}
+                  dotColor={colorsArray !== undefined ? colorsArray[i] : '#356CB1'}
+                />
+                </TouchableOpacity>
+            );
         }
 
         return dots;
     }
 
     render () {
-        const { dotsLength, containerStyle, vertical } = this.props;
+        const { dotsLength, containerStyle } = this.props;
 
         if (!dotsLength || dotsLength < 2) {
             return false;
         }
 
-        const style = [
-            styles.sliderPagination,
-            { flexDirection: vertical ? 'column' : 'row' },
-            containerStyle || {}
-        ];
-
         return (
-            <View pointerEvents={'box-none'} style={style}>
+            <View>
+            <ScrollView
+            horizontal={true}
+              pointerEvents={'none'}
+              contentContainerStyle={[styles.sliderPagination, containerStyle || {}]}
+            >
                 { this.dots }
+            </ScrollView>
             </View>
         );
     }
--- src/pagination/Pagination.style.js
+++ src/pagination/Pagination.style.js
@@ -1,14 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 const DEFAULT_DOT_SIZE = 7;
-const DEFAULT_DOT_COLOR = 'rgba(0, 0, 0, 0.75)';
+const DEFAULT_DOT_COLOR = 'rgba(0, 0, 0, 0.1)';
 
 export default StyleSheet.create({
     sliderPagination: {
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        paddingHorizontal: 20,
-        paddingVertical: 30
     },
     sliderPaginationDot: {
         width: DEFAULT_DOT_SIZE,
